4a86d8201b30573a44ac5034f7ee7b57e083d75a
1parakeet1 <95216234+1parakeet1@users.noreply.github.com>
2022-08-19 23:06:58 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+function deficiency(){
+    window.location= "defi.html";
+}
+
+function herdity(){
+    window.location="heri.html";
+}
+
+function physiological(){
+    window.location="physio.html";
+}
+
+function infectious(){
+    window.location="infec.html";
+}
+function back(){
+    window.location="index.html";
+}
